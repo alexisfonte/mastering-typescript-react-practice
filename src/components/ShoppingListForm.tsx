6b736dad00cf0ae0fd3fdd30c93e9392e72e245a
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-07-17 14:24:54 -0500

    added quantity input

--- src/components/ShoppingListForm.tsx
+++ src/components/ShoppingListForm.tsx
@@ -1,20 +1,24 @@
 import React, { useRef } from "react";
 
 interface ShoppingListFormProps {
-    onAddItem: (item: string) => void;
+    onAddItem: (item: string, quantity: number) => void;
 }
 
 function ShoppingListForm({ onAddItem }: ShoppingListFormProps): JSX.Element {
-    const inputRef = useRef<HTMLInputElement>(null);
+    const productRef = useRef<HTMLInputElement>(null);
+    const quantityRef = useRef<HTMLInputElement>(null);
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
-    const newProduct = inputRef.current!.value;
-    onAddItem(newProduct)
-    inputRef.current!.value = ""
+    const newProduct = productRef.current!.value;
+    const quantity = parseInt(quantityRef.current!.value);
+    onAddItem(newProduct, quantity)
+    productRef.current!.value = ""
+    quantityRef.current!.value = "1"
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Product Name" ref={inputRef}/>
+      <input type="text" placeholder="Product Name" ref={productRef}/>
+      <input type="number" min={0} ref={quantityRef}/>
       <button type="submit">Add Item</button>
     </form>
   );
